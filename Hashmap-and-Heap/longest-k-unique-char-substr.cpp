c6716bd0404c80e5c2c47601a8a524c30eaec16a
unknown <simrankr361@gmail.com>
2019-06-25 10:22:20 +0530

    25june

--- Hashmap-and-Heap/longest-k-unique-char-substr.cpp
+++ Hashmap-and-Heap/longest-k-unique-char-substr.cpp
@@ -1,49 +1,54 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-void acquire(unordered_map<char,int> &m , int &e , string &str)
+
+int grain(int *arr,int n)
 {
-    m[str[e]]++;
-    e++;
-}
-void release(unordered_map<char,int> &m , int &s , string &str)
-{
-
-    s++;
-    m[str[s]]--;
-    if(m[str[s]] == 0)
+    pair<int,int> m ,sm;
+    m.first = -1, sm.first = -1;
+    for(int i = 0 ;i<n;i++)
     {
-        m.erase(str[s]);
+        if(arr[i] > m.first)
+        {
+           //cout<<m.first<<endl;
+            m.first = arr[i];
+            m.second = i;
+        }
     }
-
-}
-void print(string &str, int k) {
-    int s = -1, e = 0 , ml = -1;
-    unordered_map<char , int> m;
-    while( s+1 < str.length() && e<str.length())
+    for(int i = 0 ; i < n;i++)
     {
-            while(m.size() <= k)
-            {
-                    acquire(m,e,str);
-            }
-
-            //settle
-            ml = max(ml , e-s -2);
-
-            while(m.size() > k)
-            {
-                release(m,s,str);
-            }
+        if(arr[i] == m.first && i != m.second)
+        {
+            sm.first = arr[i];
+            sm.second = i;
+        }
+        else if(arr[i] > sm.first && i != m.second )
+        {
+            sm.first = arr[i];
+            sm.second = i;
+        }
     }
-    cout<<ml;
- }
-
- int main(int argc,char** argv){
-     string s;
-     cin>>s;
-     int k;
-     cin>>k;
-     print(s,k);
- }
+//cout<<m.first<<m.second<< " "<<sm.first<<sm.second<<endl;
+    int s = min(m.second,sm.second) , e = max(m.second,sm.second);
+    int h = sm.first,r = 0;
+    for(int i = s+1 ; i < e ; i++ )
+    {
+        r += (h-arr[i]);
+    }
+    return r;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    int a[n];
+        for(int i = 0 ; i < n ; i++)
+        {
+            cin>>a[i];
+        }
+    cout<<grain(a,n);
+    return 0;
+}
