3654e5dff87ec0bfc21b4b8b0bfd6b48a654dc3f
unknown <simrankr361@gmail.com>
2019-07-04 15:20:53 +0530

    4july

---
+++ Arrays-and-Strings/k-concat.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int kadane(vector<int> v)
+{
+    int currmax = 0 , maxsofar = INT_MIN;
+    for(int i = 0 ; i  < v.size() ; i++)
+    {
+        if(currmax>=0)
+        {
+            currmax += v[i];
+        }
+        else
+        {
+            currmax = v[i];
+        }
+        maxsofar = max(currmax , maxsofar);
+    }
+    return maxsofar;
+}
+int kconcat(vector<int> v , int n , int k)
+{
+    int s = 0;
+    for(int i = 0 ; i  < n ;i++)
+    {
+        s += v[i];
+    }
+    vector<int> concat(2*n);
+    int i;
+    for( i = 0 ; i  < n ; i ++)
+    {
+        concat[i] = v[i];
+    }
+    for(  ; i  < 2*n ; i ++)
+    {
+        concat[i] = v[i-n];
+    }
+    if(s > 0)
+    {
+        return (k-2)*s + kadane(concat);
+    }
+    else
+    {
+        return kadane(concat);
+    }
+}
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    vector<int> v(n);
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin>>v[i];
+    }
+    cout<<kconcat(v,n,k);
+    return 0;
+}
