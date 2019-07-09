0701fb08a0e2bc6bc7c63756e7a9a5a84547bcc4
unknown <simrankr361@gmail.com>
2019-07-10 00:47:50 +0530

    10july

---
+++ Dynamic-Programming/egg-drop-dp.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/* Function to get minimum number of trials needed in worst
+case with n eggs and k floors */
+int eggDrop(int n, int k)
+{
+    int dp[k+1][n+1];
+    memset(dp,0,sizeof(dp));
+    for(int j = 0 ; j < n+1; j++)
+    {
+        dp[1][j] = j;
+    }
+    for(int i=1;i<k+1;i++)
+    {
+        dp[i][1] = 1;
+    }
+
+    for(int i = 2; i < k+1 ; i++)
+    {
+
+       for(int j=2;j<n+1;j++)
+       {
+           int mans = INT_MIN;
+           int m = INT_MAX;
+           for(int f = 1 ; f <= j ; f++)
+           {
+               mans = max(dp[i-1][f-1] , dp[i][j-f]);
+               m = min( m, mans);
+           }
+           dp[i][j] = m+1;
+       }
+    }
+
+    for(int i=0;i<k+1;i++)
+    {
+        for(int j=0;j<n+1;j++)
+        {
+            cout<<dp[i][j]<<" " ;
+        }
+        cout<<endl;
+    }
+    return dp[k][n];
+}
+
+/* Driver program to test to pront printDups*/
+int main()
+{
+	int n = 2, k = 36;
+	cout<< eggDrop(n, k);
+	return 0;
+}
