3f237aa182c0c79fc82a56926504df2353c39666
unknown <simrankr361@gmail.com>
2019-07-14 15:48:39 +0530

    14june

--- Dynamic-Programming/max-sum-no-adj-elements.cpp
+++ Dynamic-Programming/max-sum-no-adj-elements.cpp
@@ -12,7 +12,7 @@ int maxloot(int *  arr ,  int n)
     for(int i = 1;i<n;i++)
     {
         dp[0][i] = dp[1][i-1] + arr[i];
-        dp[1][i] = dp[0][i-1];
+        dp[1][i] =max( dp[0][i-1] , dp[1][i-1]);
     }
     return max(dp[0][n-1] , dp[1][n-1]);
 }
