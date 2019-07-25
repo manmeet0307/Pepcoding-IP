aa093b1ccbdab3a54f900d41b9070f99eebf3100
unknown <simrankr361@gmail.com>
2019-07-26 03:46:34 +0530

    26july

---
+++ trees/upside-down.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+ #include<vector>
+ #include<bits/stdc++.h>
+ using namespace std;
+
+ // TreeNode class for a node of a Binary Search Tree
+ class TreeNode {
+     public:
+ 	 int val;
+ 	 TreeNode* left;
+ 	 TreeNode* right;
+
+ 	 TreeNode(int x) {
+ 	 	 val = x;
+         left = NULL;
+         right = NULL;
+ 	 }
+ };
+
+ class BinaryTree {
+     public:
+ 	 TreeNode* upsideDown(TreeNode* node) {
+ 	 	TreeNode * c = node;
+ 	 	TreeNode * x = node;
+ 	 	TreeNode* p = NULL , *tbrc = NULL;
+ 	 	while(c!=NULL)
+        {
+            x = c;
+            TreeNode * rl = c->left , *temp = c->right;
+            c->right = tbrc ;
+            c->left = p;
+            tbrc = c;
+            p = temp;
+            c = rl;
+
+        }
+        return x;
+ 	 }
+
+ 	 // Don't make any changes here
+ 	 void inOrder(TreeNode* node) {
+ 	 	 if (node == NULL) {
+ 	 	 	 return;
+ 	 	 }
+
+ 	 	 inOrder(node->left);
+ 	 	 cout<<node->val<<" ";
+ 	 	 inOrder(node->right);
+ 	 }
+ };
+
+ //Don't change code of utility function.
+ TreeNode* stringToTreeNode(string input) {
+     int first = input.find_first_not_of(' ');
+     int last = input.find_last_not_of(' ');
+     input = input.substr(first, (last - first + 1));
+     if (!input.size()) {
+         return nullptr;
+     }
+
+     string item;
+     stringstream ss;
+     ss.str(input);
+
+     getline(ss, item, ' ');
+     TreeNode* root = new TreeNode(stoi(item));
+     queue<TreeNode*> nodeQueue;
+     nodeQueue.push(root);
+
+     while (true) {
+         TreeNode* node = nodeQueue.front();
+         nodeQueue.pop();
+         if (!getline(ss, item, ' ')) {
+             break;
+         }
+
+         if (item != "null") {
+             int leftNumber = stoi(item);
+             node->left = new TreeNode(leftNumber);
+             nodeQueue.push(node->left);
+         }
+
+         if (!getline(ss, item, ' ')) {
+             break;
+         }
+
+         if (item != "null") {
+             int rightNumber = stoi(item);
+             node->right = new TreeNode(rightNumber);
+             nodeQueue.push(node->right);
+
+         }
+     }
+     return root;
+ }
+
+ int main(int argc, char** argv){
+     string line;
+     getline(cin, line);
+     TreeNode* root = stringToTreeNode(line);
+ 	 BinaryTree bt;
+ 	 TreeNode* newRoot = bt.upsideDown(root);
+ 	 bt.inOrder(newRoot);
+ }
