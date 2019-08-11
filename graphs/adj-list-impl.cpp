63e34c111293f15474cfc54459d1be672f1dac6f
unknown <simrankr361@gmail.com>
2019-08-11 14:33:24 +0530

    11aug

---
+++ graphs/adj-list-impl.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+class edge{
+public:
+    int nbr;
+    int wt;
+};
+vector<vector<edge>> graph;
+void addedge(int s, int d , int wt)
+{
+    edge e;
+    e.nbr = d;
+    e.wt = wt;
+    graph[s].push_back(e);;
+    edge e2;
+    e2.nbr = s;
+    e2.wt = wt;
+    graph[d].push_back(e2);
+}
+
+void display()
+{
+    for(int i = 0 ; i  < graph.size() ; i++)
+    {
+        cout<<i<<" -> ";
+        for(int j = 0 ; j < graph[i].size() ; j++)
+        {
+            cout<<"[ " <<graph[i][j].nbr<<" " <<graph[i][j].wt<< " ] ";
+        }
+        cout<<endl;
+    }
+}
+
+bool haspath(int s, int d , vector<bool> &visited)
+{
+    if(s == d)
+    {
+        return true;
+    }
+    visited[s] = true;
+    for(int i = 0; i < graph[s].size() ; i++)
+    {
+        if(!visited[graph[s][i].nbr])
+        {
+            bool pfromnbr = haspath(graph[s][i].nbr , d , visited);
+            if(pfromnbr)
+                return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+    graph.push_back(vector<edge>());
+
+    addedge(0,1,10);
+    addedge(1,2,10);
+    addedge(2,3,10);
+    addedge(3,0,40);
+    addedge(3,4,2);
+    addedge(4,5,3);
+    addedge(5,6,3);
+    addedge(6,4,8);
+
+    display();
+    vector<bool> visited(7,0);
+    cout<<haspath(0 , 6 , visited);
+
+}
